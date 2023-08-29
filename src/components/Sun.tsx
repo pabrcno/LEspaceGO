8000143645547804ec9a92c57fc4fac04c917396
pabrcno <pabrcn@gmail.com>
2023-08-29 01:37:10 -0300

    improve illumination

--- src/components/Sun.tsx
+++ src/components/Sun.tsx
@@ -7,6 +7,8 @@ import { useRef } from "react";
 export const Sun = () => {
   const texture = useLoader(TextureLoader, ["/sun.jpg"]) as THREE.Texture[];
   const sunRef = useRef<Group>(null);
+  const lightRef = useRef<THREE.PointLight>(null);
+
   useFrame(() => {
     if (sunRef.current) {
       sunRef.current.rotation.y += 0.0001;
@@ -15,13 +17,19 @@ export const Sun = () => {
   return (
     <group ref={sunRef}>
       <Sparkles speed={0.5} scale={[40, 40, 40]} size={20} count={25} />
-      <pointLight position={[0, 35, 0]} intensity={0.5} />
+      <pointLight
+        position={[0, 35, 0]}
+        intensity={0.5}
+        distance={1000}
+        ref={lightRef}
+      />
 
-      <pointLight position={[35, 0, 0]} intensity={0.5} />
-      <pointLight position={[-35, 0, 0]} intensity={0.5} />
-      <pointLight position={[0, -35, 0]} intensity={0.5} />
-      <pointLight position={[0, 0, 35]} intensity={0.5} />
-      <pointLight position={[0, 0, -35]} intensity={0.5} />
+      <pointLight position={[35, 0, 0]} intensity={1} distance={40} />
+      <pointLight position={[-35, 0, 0]} intensity={1} distance={40} />
+      <pointLight position={[0, -35, 0]} intensity={1} distance={40} />
+      <pointLight position={[0, 0, 35]} intensity={1} distance={40} />
+      <pointLight position={[0, 0, -35]} intensity={1} distance={40} />
+
       <Planet
         opacity={0.9}
         scale={6}
